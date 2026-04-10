9578b82ca65834a05ffcedaf2e65115fa9ebc756
RedaSarhani <redasarhani02@gmail.com>
2026-04-10 18:56:17 +0100

    feat: initialize Spring Boot backend project with Maven wrapper

---
+++ smarthr-backend/src/main/java/com/smarthr/smarthr_backend/SmarthrBackendApplication.java
@@ -0,0 +1,13 @@
+package com.smathr.smarthr_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmarthrBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmarthrBackendApplication.class, args);
+	}
+
+}
---
+++ smarthr-backend/src/test/java/com/smarthr/smarthr_backend/SmarthrBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smathr.smarthr_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmarthrBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
