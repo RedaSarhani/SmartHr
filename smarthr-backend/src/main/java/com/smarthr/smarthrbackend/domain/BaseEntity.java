588cebeaa3d3ed443de909cd19f84881b9493843
RedaSarhani <redasarhani02@gmail.com>
2026-04-12 12:26:19 +0100

    feat: add BaseEntity

---
+++ smarthr-backend/src/main/java/com/smarthr/smarthrbackend/domain/BaseEntity.java
@@ -0,0 +1,30 @@
+package com.smarthr.smarthrbackend.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID id;
+
+    @CreationTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+}
